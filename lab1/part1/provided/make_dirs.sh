773c7b051282086bf5b569745d3416b2661b9971
Rajarshi Roy <10099786+iMrsRoy@users.noreply.github.com>
2025-01-26 14:50:15 +0000

    lab1/part1-done

--- lab1/part1/provided/make_dirs.sh
+++ lab1/part1/provided/make_dirs.sh
@@ -2,4 +2,11 @@ mkdir lab-host1
 mkdir lab-host2
 mkdir lab-host3
 mkdir lab-host4
+# Check for each directory and create only if it doesn't exist
+[ ! -d "lab-host1" ] && mkdir lab-host1
+[ ! -d "lab-host2" ] && mkdir lab-host2
+[ ! -d "lab-host3" ] && mkdir lab-host3
+[ ! -d "lab-host4" ] && mkdir lab-host4
+[ ! -d "lab-switch" ] && mkdir lab-switch
+
 mkdir lab-switch
